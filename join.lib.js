3697a4d9998deb8549f6ebe720c181e02f823b52
darkredglow <10967016+darkredglow@users.noreply.github.com>
2018-10-04 15:50:24 -0400

    Update join.lib.js

--- join.lib.js
+++ join.lib.js
@@ -264,6 +264,12 @@ JoinForm.prototype.validateForm = function () {
             if(error_count == 1){
             	$s(fieldName).focus();
             }
+		$s(fieldName + "_error").setAttribute("role", "alert");
+            }
+            $s(fieldName + "_error").firstChild.nodeValue = error.message;
+            if(error_count == 1){
+            	$s(fieldName).focus();
+            }
         }
     	
     }
